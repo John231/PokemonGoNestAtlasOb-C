a84894b4e8c5694f0b923fb3482303cd30e64a15
John231 <[j_b_93@hotmail.co.uk]>
2017-06-08 10:46:39 +0100

    Added initial location code to initialise to York UK

--- NestAtlasOb-C/ViewController.m
+++ NestAtlasOb-C/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -17,6 +20,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 53.960024;
+    zoomLocation.longitude = -1.087152;
+    
+    CLLocationDistance regionRadius = 1000.0;
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, regionRadius*2.0, regionRadius*2.0);
+    
+    [_mapView setRegion:viewRegion animated:YES];
 }
 
 
